5b9b225a17df07f6255e55586a612ede829cd880
PedroVasconcelos18 <48729314+PedroVasconcelos18@users.noreply.github.com>
2019-10-20 19:38:39 -0300

    Add files via upload

--- db/pit.sql
+++ db/pit.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: 19-Set-2019 às 00:14
+-- Generation Time: 25-Set-2019 às 21:53
 -- Versão do servidor: 5.7.26
 -- versão do PHP: 7.2.18
 
@@ -25,6 +25,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `administrador`
+--
+
+DROP TABLE IF EXISTS `administrador`;
+CREATE TABLE IF NOT EXISTS `administrador` (
+  `nome` varchar(150) NOT NULL,
+  `senha` varchar(150) NOT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `administrador`
+--
+
+INSERT INTO `administrador` (`nome`, `senha`, `ID`) VALUES
+('admin', '202cb962ac59075b964b07152d234b70', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `aposentadoautonomo`
 --
 
@@ -39,6 +60,7 @@ CREATE TABLE IF NOT EXISTS `aposentadoautonomo` (
   `senha` varchar(50) NOT NULL,
   `numero` varchar(5) NOT NULL,
   `tipo_usuario` int(11) NOT NULL,
+  `dataCadastro` date DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
@@ -46,11 +68,26 @@ CREATE TABLE IF NOT EXISTS `aposentadoautonomo` (
 -- Extraindo dados da tabela `aposentadoautonomo`
 --
 
-INSERT INTO `aposentadoautonomo` (`ID`, `CPF`, `dataNasc`, `Nome`, `CEP`, `Email`, `senha`, `numero`, `tipo_usuario`) VALUES
-(2, '22222222222', '2000-08-02', 'Roger', '31080150', 'pedro@pedro.com', 'e10adc3949ba59abbe56e057f20f883e', '703', 0),
-(3, '11111111111', '1988-09-22', 'teste', '31080-150', 'pedrohvss15@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '123', 0),
-(4, '11204967644', '2000-08-02', 'Pedro Henrique Vasconcelos ', '31080150', 'pedrohvss15@gmail.com', '202cb962ac59075b964b07152d234b70', '703', 0),
-(5, '11204967644', '2000-08-02', 'Pedro Henrique Vasconcelos ', '31080150', 'pedrohvss15@gmail.com', '0684e0571f251647d0afdf3ee5194b98', '703', 0);
+INSERT INTO `aposentadoautonomo` (`ID`, `CPF`, `dataNasc`, `Nome`, `CEP`, `Email`, `senha`, `numero`, `tipo_usuario`, `dataCadastro`) VALUES
+(2, '22222222222', '2000-08-02', 'Roger', '31080150', 'pedro@pedro.com', 'e10adc3949ba59abbe56e057f20f883e', '703', 1, '2019-09-19'),
+(3, '11111111111', '1988-09-22', 'teste', '31080-150', 'pedrohvss15@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '123', 1, '2019-09-19'),
+(4, '11204967644', '2000-08-02', 'Pedro Henrique Vasconcelos ', '31080150', 'pedrohvss15@gmail.com', '202cb962ac59075b964b07152d234b70', '703', 1, '2019-09-19'),
+(5, '11204967644', '2000-08-02', 'Pedro Henrique Vasconcelos ', '31080150', 'pedrohvss15@gmail.com', '0684e0571f251647d0afdf3ee5194b98', '703', 1, '2019-09-19');
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `aposentados_autonomos_cadastrados`
+-- (See below for the actual view)
+--
+DROP VIEW IF EXISTS `aposentados_autonomos_cadastrados`;
+CREATE TABLE IF NOT EXISTS `aposentados_autonomos_cadastrados` (
+`ID` int(11)
+,`CPF` varchar(45)
+,`Nome` varchar(150)
+,`Email` varchar(45)
+,`dataCadastro` date
+);
 
 -- --------------------------------------------------------
 
@@ -104,14 +141,30 @@ CREATE TABLE IF NOT EXISTS `empresa` (
   `senha` varchar(50) NOT NULL,
   `tipo_usuario` int(11) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `empresa`
 --
 
 INSERT INTO `empresa` (`ID`, `CNPJ`, `nome`, `CEP`, `telefone`, `ramoEmpresa`, `email`, `senha`, `tipo_usuario`) VALUES
-(2, '87.286.520/0001-92', 'Pedro Henrique Vasconcelos ', '31080150', '31995621858', 'Vendas', 'pedrohvss15@gmail.com', '202cb962ac59075b964b07152d234b70', 1);
+(2, '87.286.520/0001-92', 'Empresa1', '31080150', '31995621858', 'Vendas', 'pedrohvss15@gmail.com', '202cb962ac59075b964b07152d234b70', 1),
+(3, '87.286.520/0001-93', 'Empresa2', '31080150', '31995621858', 'Vendas', 'pedrohvss15@gmail.com', '202cb962ac59075b964b07152d234b70', 1),
+(4, '87.286.520/0001-94', 'Empresa3', '31080150', '31995621858', 'Programação', 'pedrohvss15@gmail.com', '202cb962ac59075b964b07152d234b70', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `empresas_cadastradas`
+-- (See below for the actual view)
+--
+DROP VIEW IF EXISTS `empresas_cadastradas`;
+CREATE TABLE IF NOT EXISTS `empresas_cadastradas` (
+`ID_da_empresa` int(11)
+,`Nome_da_empresa` varchar(45)
+,`Telefone_da_empresa` varchar(45)
+,`Ramo_da_empresa` varchar(45)
+);
 
 -- --------------------------------------------------------
 
@@ -127,7 +180,33 @@ CREATE TABLE IF NOT EXISTS `inscricao` (
   PRIMARY KEY (`ID`),
   KEY `fk_Vagas_has_AposentadoAutonomo_AposentadoAutonomo1_idx` (`AposentadoAutonomo_ID`),
   KEY `fk_Vagas_has_AposentadoAutonomo_Vagas1_idx` (`Vagas_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `inscricao`
+--
+
+INSERT INTO `inscricao` (`ID`, `Vagas_ID`, `AposentadoAutonomo_ID`) VALUES
+(1, 9, 4),
+(2, 9, 4),
+(3, 9, 4),
+(4, 9, 4),
+(5, 9, 4),
+(6, 9, 4),
+(7, 9, 4),
+(8, 9, 4),
+(9, 9, 4),
+(10, 9, 4),
+(11, 9, 4),
+(12, 9, 4),
+(13, 9, 4),
+(14, 9, 4),
+(15, 9, 4),
+(16, 9, 4),
+(17, 9, 4),
+(18, 10, 4),
+(20, 9, 4),
+(21, 13, 4);
 
 -- --------------------------------------------------------
 
@@ -157,7 +236,6 @@ DROP TABLE IF EXISTS `vagas`;
 CREATE TABLE IF NOT EXISTS `vagas` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `numeroVagas` int(11) NOT NULL,
-  `endereco` varchar(80) NOT NULL,
   `Sexo` varchar(45) NOT NULL,
   `valorBolsa` double NOT NULL,
   `Contato` varchar(45) NOT NULL,
@@ -169,10 +247,47 @@ CREATE TABLE IF NOT EXISTS `vagas` (
   `funcaoExercida` varchar(150) NOT NULL,
   `DuracaoVaga` varchar(150) NOT NULL,
   `categoria` varchar(45) NOT NULL,
+  `CEP` varchar(45) NOT NULL,
+  `numeroCasa` varchar(5) NOT NULL,
   `Empresa_ID` int(11) NOT NULL,
   PRIMARY KEY (`ID`),
   KEY `fk_Vagas_Empresa_idx` (`Empresa_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `vagas`
+--
+
+INSERT INTO `vagas` (`ID`, `numeroVagas`, `Sexo`, `valorBolsa`, `Contato`, `conhecimentosNecessarios`, `Beneficios`, `Observacoes`, `HorarioTrabalho`, `nomeEmpresa`, `funcaoExercida`, `DuracaoVaga`, `categoria`, `CEP`, `numeroCasa`, `Empresa_ID`) VALUES
+(9, 1, 'H', 2500, '31995621858', 'Saber dirigir', 'Vale-transporte, vale alimentação', 'Preciso de alguÃ©m experiente no assunto', '12:00 atÃ© as 22:00', 'TesteNovoCep', 'Motorista', '3 anos', 'Coz', '31080150', '703', 2),
+(10, 1, 'H', 2500, '31995621858', 'Saber dirigir', 'Vale-transporte, vale alimentação', 'Preciso de alguÃ©m experiente no assunto', '12:00 atÃ© as 22:00', 'TesteNovoCep', 'Motorista', '3 anos', 'Coz', '31080150', '703', 2),
+(12, 1, 'H', 2500, '31995621858', 'Saber dirigir', 'Vale-transporte, vale alimentação', 'Preciso de alguÃ©m experiente no assunto', '12:00 atÃ© as 22:00', 'apue', 'Motorista', '3 anos', 'Coz', '31080150', '703', 2),
+(13, 1, 'H', 2500, '31995621858', 'Saber dirigir', 'Vale-transporte, vale alimentação', 'Preciso de alguÃ©m experiente no assunto', '12:00 atÃ© as 22:00', 'apue1', 'Motorista', '3 anos', 'Adminis', '31080150', '703', 2),
+(14, 1, 'H', 2500, '31995621858', 'Saber dirigir', 'Vale-transporte, vale alimentação', 'Preciso de alguÃ©m experiente no assunto', '12:00 atÃ© as 22:00', 'apue2', 'Motorista', '3 anos', 'Limp', '31080150', '703', 2),
+(15, 1, 'I', 2500, '31995621858', 'Saber dirigir', 'Vale-transporte, vale alimentação', 'Preciso de alguÃ©m experiente no assunto', '12:00 atÃ© as 22:00', 'apue3', 'Motorista', '3 anos', 'Aux', '31080150', '703', 2),
+(16, 1, 'I', 2500, '31995621858', 'Saber dirigir', 'Vale-transporte, vale alimentação', 'Preciso de alguÃ©m experiente no assunto', '12:00 atÃ© as 22:00', 'apue4', 'Motorista', '3 anos', 'Pedg', '31080150', '703', 2),
+(17, 1, 'I', 2500, '31995621858', 'Saber dirigir', 'Vale-transporte, vale alimentação', 'Preciso de alguÃ©m experiente no assunto', '12:00 atÃ© as 22:00', 'apue5', 'Motorista', '3 anos', 'Prog', '31080150', '703', 2),
+(18, 1, 'I', 2500, '31995621858', 'Saber dirigir', 'Vale-transporte, vale alimentação', 'Preciso de alguÃ©m experiente no assunto', '12:00 atÃ© as 22:00', 'apue6', 'Motorista', '3 anos', 'Infra', '31080150', '703', 2),
+(19, 1, 'I', 2500, '31995621858', 'Saber dirigir', 'Vale-transporte, vale alimentação', 'Preciso de alguÃ©m experiente no assunto', '12:00 atÃ© as 22:00', 'apue7', 'Motorista', '3 anos', 'Construc', '31080150', '703', 2),
+(20, 1, 'I', 2500, '31995621858', 'Saber dirigir', 'Vale-transporte, vale alimentação', 'Preciso de alguÃ©m experiente no assunto', '12:00 atÃ© as 22:00', 'apue9', 'Motorista', '3 anos', 'Ven', '31080150', '703', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `aposentados_autonomos_cadastrados`
+--
+DROP TABLE IF EXISTS `aposentados_autonomos_cadastrados`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `aposentados_autonomos_cadastrados`  AS  select `aposentadoautonomo`.`ID` AS `ID`,`aposentadoautonomo`.`CPF` AS `CPF`,`aposentadoautonomo`.`Nome` AS `Nome`,`aposentadoautonomo`.`Email` AS `Email`,`aposentadoautonomo`.`dataCadastro` AS `dataCadastro` from `aposentadoautonomo` ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `empresas_cadastradas`
+--
+DROP TABLE IF EXISTS `empresas_cadastradas`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `empresas_cadastradas`  AS  select `empresa`.`ID` AS `ID_da_empresa`,`empresa`.`nome` AS `Nome_da_empresa`,`empresa`.`telefone` AS `Telefone_da_empresa`,`empresa`.`ramoEmpresa` AS `Ramo_da_empresa` from `empresa` ;
 
 --
 -- Constraints for dumped tables
